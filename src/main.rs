ec18833bd86db3a3649c8fcbce609d170db01de0
vickyone0 <vigneshs0.618@gmail.com>
2025-04-30 16:48:32 +0530

    buffer overflow

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use std::thread;
 fn main() {
-let mut data = 100;
-
-thread::spawn(|| { data = 500; });
-thread::spawn(|| { data = 1000; });
- println!("{}", data);
- }
+  let fruit = vec![' ', ' ', ' '];
+  
+   let buffer_overflow = fruit[4];
+  assert_eq!(buffer_overflow, ' ')
+  }
