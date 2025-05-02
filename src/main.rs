7cbbe8192cc81b6029b2f1b2cbdc723adda5209d
vickyone0 <vigneshs0.618@gmail.com>
2025-05-02 17:30:57 +0530

    Rc<RefCell<>> for mutable value of object

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 use std::rc::Rc;
+use std::cell::RefCell;
 
 #[derive(Debug)]
-struct GroundStation {}
+struct GroundStation {
+    radio_freq:f64,
+}
 
 fn main() {
-    let base = Rc::new(GroundStation {});
+    let base = Rc::new(RefCell::new(GroundStation { radio_freq:87.0}));
+
     println!("{:?}",base);
+
+    {
+        let mut base_2 = base.borrow_mut();
+        base_2.radio_freq -= 15.6;
+        println!("base_2: {:?}",base_2);
+        println!("base: {:?}",base);
+    }
+
+    println!("base: {:?}",base);
+
+    
+        let mut base_3 = base.borrow_mut();
+        base_3.radio_freq += 15.6;
+        println!("base_3: {:?}",base_3);
+        println!("base: {:?}",base);
+    
 }
