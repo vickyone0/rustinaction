5b28bcb6a3b72f376c3017cbd67478b1202dc1c2
vickyone0 <vigneshs0.618@gmail.com>
2025-05-02 17:10:22 +0530

    Rc immutable with a counter

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,9 @@
-#[derive(Debug, Clone, Copy)]
-struct Cubsat {
-    cub_id: u32,
-}
+use std::rc::Rc;
 
+#[derive(Debug)]
+struct GroundStation {}
 
-#[derive(Debug, Clone, Copy)]
-enum StatusMessage {
-    Ok,
-}
-
-fn check_status(cub_id:Cubsat) -> StatusMessage{
-    StatusMessage::Ok
-}
-
-fn  main() {
-    let cub_id = Cubsat { cub_id: 1 };
-    let status = check_status(cub_id.clone());
-    println!("{:?}", status.clone());
-
-    let status = check_status(cub_id);
-     println!("{:?}", status);
+fn main() {
+    let base = Rc::new(GroundStation {});
+    println!("{:?}",base);
 }
