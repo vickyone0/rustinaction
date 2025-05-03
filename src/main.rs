6ee9c86e34b77b99f8c48d0481630b465f2292b7
vickyone0 <vigneshs0.618@gmail.com>
2025-05-03 17:19:24 +0530

    builder for optional parameter in struct

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,70 @@
-use std::path::Path;
 
 #[derive(Debug)]
-pub enum Error {
-    Io(std::io::Error),
-    BadLineArgument(usize),
+struct Bicycle {
+    make: String,
+    model: String,
+    size:i32,
+    color: String,
 }
 
-impl From<std::io::Error> for Error {
-    fn from(err: std::io::Error) -> Self {
-        Self::Io(err)
+impl Bicycle {
+    fn make(&self) -> &String {
+        &self.make
     }
-    
+    fn model(&self) -> &String {
+        &self.model
+    }
+    fn size(&self) -> i32 {
+        self.size
+    }
+    fn color(&self) -> &String {
+        &self.color
+    }
 }
 
-fn read_nth_line(path: &Path, n: usize) -> Result<String, Error> {
-    
 
-    if n < 1 {
-        return Err(Error::BadLineArgument(n));
+struct BicycleBuilder {
+    bicycle: Bicycle,
+}
+
+impl BicycleBuilder {
+    fn new() -> Self {
+        Self {
+            bicycle: Bicycle {
+                make: String::new(),
+                model: String::new(),
+                size: 0,
+                color: String::new(),
+            },
+        }
     }
-    use std::fs::File;
-    use std::io::{BufRead, BufReader};
-    let file = File::open(path)?;
 
-    let mut reader_lines = BufReader::new(file).lines();
+    fn with_make(&mut self, make:&str) {
+        self.bicycle.make = make.into()
+    }
 
-    reader_lines.nth(n-1).map(|result| result.map_err(|err| err.into())).unwrap_or_else(|| Err(Error::BadLineArgument(n)))
+    fn with_model(&mut self, model:&str) {
+        self.bicycle.model = model.into()
+    }
+
+    fn with_size(&mut self, size:i32) {
+        self.bicycle.size = size
+    }
+    fn with_color(&mut self, color:&str) {
+        self.bicycle.color = color.into()
+    }
+    fn build(self) -> Bicycle {
+        self.bicycle
+    }
+
 }
 
 fn main() {
-    let path = Path::new("Cargo.toml");
-
-    println!("The 4th line of cargo.toml is:{}", read_nth_line(path, 4).unwrap());
+    let mut bicycle_builder = BicycleBuilder::new();
+bicycle_builder.with_make("Huffy");
+bicycle_builder.with_model("Radio");
+bicycle_builder.with_size(46);
+bicycle_builder.with_color("red");
+let bicycle = bicycle_builder.build();
+println!("My new bike: {:#?}", bicycle); 
 }
-
-
-
