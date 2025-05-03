2d777f555dafbda276f277bca00f92de7bbc3715
vickyone0 <vigneshs0.618@gmail.com>
2025-05-03 15:24:41 +0530

    file reading

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,38 @@
-use std::rc::Rc;
-use std::cell::RefCell;
+use std::path::Path;
 
 #[derive(Debug)]
-struct GroundStation {
-    radio_freq:f64,
+pub enum Error {
+    Io(std::io::Error),
+    BadLineArgument(usize),
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Self::Io(err)
+    }
+    
+}
+
+fn read_nth_line(path: &Path, n: usize) -> Result<String, Error> {
+    
+
+    if n < 1 {
+        return Err(Error::BadLineArgument(n));
+    }
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+    let file = File::open(path)?;
+
+    let mut reader_lines = BufReader::new(file).lines();
+
+    reader_lines.nth(n-1).map(|result| result.map_err(|err| err.into())).unwrap_or_else(|| Err(Error::BadLineArgument(n)))
 }
 
 fn main() {
-    let base = Rc::new(RefCell::new(GroundStation { radio_freq:87.0}));
+    let path = Path::new("Cargo.toml");
 
-    println!("{:?}",base);
-
-    {
-        let mut base_2 = base.borrow_mut();
-        base_2.radio_freq -= 15.6;
-        println!("base_2: {:?}",base_2);
-        println!("base: {:?}",base);
-    }
-
-    println!("base: {:?}",base);
-
-    
-        let mut base_3 = base.borrow_mut();
-        base_3.radio_freq += 15.6;
-        println!("base_3: {:?}",base_3);
-        println!("base: {:?}",base);
-    
+    println!("The 4th line of cargo.toml is:{}", read_nth_line(path, 4).unwrap());
 }
+
+
+
